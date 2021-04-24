d8e00757481414396fdffe00eccde4977a82a22a
idrizovdjem <idrizovdjem@gmail.com>
2021-04-24 20:47:37 +0300

    Comment game board logic

--- front-end/src/components/Game/GameBoard/GameBoard.js
+++ front-end/src/components/Game/GameBoard/GameBoard.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { Fragment, useState } from 'react';
 import classes from './GameBoard.module.css';
 
 import numberService from '../../../services/numberService.js';
@@ -12,6 +12,7 @@ const GameBoard = (props) => {
     const [selectedBox, setSelectedBox] = useState(null);
 
     const addNewRowHandle = () => {
+        // generate new row of numbers and add it to the game board
         const newRow = numberService.generateRow();
         setGameBoard(oldGameBoard => {
             const rows = oldGameBoard.map(row => row);
@@ -25,15 +26,19 @@ const GameBoard = (props) => {
             // select first box
             setSelectedBox({ row, col });
         } else {
+            // if a box is already selected
+
+            // check if the newly selected box is the same as the first selected box
             if (selectedBox.row === row && selectedBox.col === col) {
                 setSelectedBox({ row, col });
                 return;
             }
 
-            // select second box
+            // get box indecies as 1D array
             const firstBoxIndex = 10 * selectedBox.row + selectedBox.col;
             const secondBoxIndex = 10 * row + col;
 
+            // find the first and second box by row and column
             let lowerRow, lowerColumn, higherRow, higherColumn;
             if (firstBoxIndex > secondBoxIndex) {
                 lowerRow = row;
@@ -47,14 +52,17 @@ const GameBoard = (props) => {
                 higherColumn = col;
             }
 
+            // get the box values
             const firstValue = gameBoard[lowerRow][lowerColumn];
             const secondValue = gameBoard[higherRow][higherColumn];
 
+            // check if the boxes values are not equal or their sum is not equal to 10
             if (firstValue !== secondValue && firstValue + secondValue !== 10) {
                 setSelectedBox({ row, col });
                 return;
             }
 
+            // check if the pair is possible horizontaly
             let isPossibleHorizontaly = true;
 
             let firstRow = lowerRow;
@@ -62,17 +70,22 @@ const GameBoard = (props) => {
 
             // check horizontaly
             while (true) {
+                // check if the firstCol is outside the array length
                 if (firstCol === 10) {
                     firstCol = 0;
                     firstRow++;
                 }
 
+                // check if the first and highest coordinates are equal
                 if (firstRow === higherRow && firstCol === higherColumn) {
                     break;
                 }
 
+                // get current box value
                 const currentValue = gameBoard[firstRow][firstCol];
+                // if the current box value is 0, then continue
                 if (currentValue !== 0) {
+                    // otherwise the pair is not possible horizontaly
                     isPossibleHorizontaly = false;
                     break;
                 }
@@ -80,7 +93,9 @@ const GameBoard = (props) => {
                 firstCol++;
             }
 
+            // if the pair is possible horizontaly
             if (isPossibleHorizontaly) {
+                // update the game board by setting both values to 0
                 setGameBoard(oldGameBoard => {
                     const newGameBoard = oldGameBoard.slice();
                     newGameBoard[selectedBox.row][selectedBox.col] = 0;
@@ -88,11 +103,13 @@ const GameBoard = (props) => {
                     return newGameBoard;
                 });
 
+                // update score and set selected box to null
                 props.updateScore(firstValue + secondValue);
                 setSelectedBox(null);
                 return;
             }
 
+            // check if both boxes columns are same
             if (lowerColumn !== higherColumn) {
                 setSelectedBox(null);
                 return;
@@ -100,18 +117,21 @@ const GameBoard = (props) => {
 
             let isPossibleVerticaly = true;
 
-            // check verticaly
+             // check verticaly
             firstRow = lowerRow;
             firstCol = lowerColumn;
             for (let i = firstRow + 1; i < higherRow; i++) {
+                // get the current box value
                 const currentValue = gameBoard[i][firstCol];
                 if (currentValue !== 0) {
+                    // if the current box value is not 0(empty), then it's the pair is not possible verticaly
                     isPossibleVerticaly = false;
                     break;
                 }
             }
 
             if (isPossibleVerticaly) {
+                // if the pair is possible verticaly, update the game board
                 setGameBoard(oldGameBoard => {
                     const newGameBoard = oldGameBoard.slice();
                     newGameBoard[selectedBox.row][selectedBox.col] = 0;
@@ -119,6 +139,7 @@ const GameBoard = (props) => {
                     return newGameBoard;
                 });
 
+                // update score and set selected box to null
                 props.updateScore(firstValue + secondValue);
                 setSelectedBox(null);
                 return;
@@ -130,6 +151,7 @@ const GameBoard = (props) => {
     }
 
     const clearEmptyRows = () => {
+        // filter the game board with only rows that are not empty(contain at least 1 digit different than 0)
         setGameBoard(oldGameBoard => {
             const rows = oldGameBoard.filter(row => row.some(num => num !== 0));
             return rows;
@@ -155,12 +177,12 @@ const GameBoard = (props) => {
     });
 
     return (
-        <>
+        <Fragment>
             <GameButtons clearRows={clearEmptyRows} addRow={addNewRowHandle} />
             <div className={classes.GameBoard}>
                 {rows}
             </div>
-        </>
+        </Fragment>
     );
 }
 
